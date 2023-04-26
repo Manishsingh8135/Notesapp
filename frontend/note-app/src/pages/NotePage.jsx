9771caa5d69fd4c94254223ae8f2ed4d86712c8a
Manishsingh8135 <manishsingh8135@gmail.com>
2023-04-26 08:23:26 +0530

    changed styling of notes and other minor changes before deployment

--- frontend/note-app/src/pages/NotePage.jsx
+++ frontend/note-app/src/pages/NotePage.jsx
@@ -68,7 +68,7 @@ const NotePage = () => {
 
                // this function handels submit functionality    
     let handleSubmit = () =>{
-            if (id!=='new' && note.body){
+            if (id!=='new' && note.body==''){
                 deleteNote()
             }else if(id!=='new'){
                 updateNote()
@@ -78,6 +78,11 @@ const NotePage = () => {
             navigate('/')
         }
 
+
+        let handleChange = (value) => {
+            setNote(note => ({ ...note, 'body': value }))
+            console.log('Handle Change:', note)
+        }
 
   return (
     <div className='note'>
@@ -93,7 +98,8 @@ const NotePage = () => {
            
            
         </div>
-        <textarea onChange={(e) => {setNote({...note,'body':e.target.value})}} defaultValue={note?note.body:""}></textarea>
+        <textarea onChange={(e) => { handleChange(e.target.value) }} value={note?.body}></textarea>
+        {/* <textarea onChange={(e) => {setNote({...note,'body':e.target.value})}} defaultValue={note?note.body:""}></textarea> */}
        
 
     </div>
@@ -103,3 +109,113 @@ const NotePage = () => {
 export default NotePage
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///copied code
+
+
+// import React, { useState, useEffect } from 'react'
+// import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
+// import { Link } from 'react-router-dom'
+
+// const NotePage = ({ match, history }) => {
+
+//     let noteId = match.params.id
+//     let [note, setNote] = useState(null)
+
+//     useEffect(() => {
+//         getNote()
+//     }, [noteId])
+
+
+//     let getNote = async () => {
+//         if (noteId === 'new') return
+
+//         let response = await fetch(`http://127.0.0.1:8000/api/notes/${noteId}/`)
+//         let data = await response.json()
+//         setNote(data)
+//     }
+
+//     let createNote = async () => {
+//         fetch(`http://127.0.0.1:8000/api/notes/`, {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(note)
+//         })
+//     }
+
+
+//     let updateNote = async () => {
+//         fetch(`http://127.0.0.1:8000/api/notes/${noteId}/`, {
+//             method: "PUT",
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(note)
+//         })
+//     }
+
+
+//     let deleteNote = async () => {
+//         fetch(`http://127.0.0.1:8000/api/notes/${noteId}/`, {
+//             method: 'DELETE',
+//             'headers': {
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//         history.push('/')
+//     }
+
+//     let handleSubmit = () => {
+//         console.log('NOTE:', note)
+//         if (noteId !== 'new' && note.body == '') {
+//             deleteNote()
+//         } else if (noteId !== 'new') {
+//             updateNote()
+//         } else if (noteId === 'new' && note.body !== null) {
+//             createNote()
+//         }
+//         history.push('/')
+//     }
+
+//     let handleChange = (value) => {
+//         setNote(note => ({ ...note, 'body': value }))
+//         console.log('Handle Change:', note)
+//     }
+
+//     return (
+//         <div className="note" >
+//             <div className="note-header">
+//                 <h3>
+//                     <ArrowLeft onClick={handleSubmit} />
+//                 </h3>
+//                 {noteId !== 'new' ? (
+//                     <button onClick={deleteNote}>Delete</button>
+//                 ) : (
+//                     <button onClick={handleSubmit}>Done</button>
+//                 )}
+
+//             </div>
+//             <textarea onChange={(e) => { handleChange(e.target.value) }} value={note?.body}></textarea>
+//         </div>
+//     )
+// }
+
+// export default NotePage
